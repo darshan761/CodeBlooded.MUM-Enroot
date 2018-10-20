1d72425acfec8e298fdb50cbe888236d5f3eade7
DamnikJain1 <jaindamnik203@gmail.com>
2018-10-20 19:31:25 +0530

    Main Activity update(User types)

--- app/src/main/java/com/example/poojan/ezcommuter/MainActivity.java
+++ app/src/main/java/com/example/poojan/ezcommuter/MainActivity.java
@@ -3,19 +3,42 @@ package com.example.poojan.ezcommuter;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.CardView;
 import android.view.View;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView redirect;
+    private CardView commuter_card;
+    private CardView traffic_officer_card;
+    private CardView ambulance_card;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        redirect = findViewById(R.id.redirect);
-        redirect.setOnClickListener(new View.OnClickListener() {
+        commuter_card = findViewById(R.id.commuter_card);
+        traffic_officer_card = findViewById(R.id.traffic_officer_card);
+        ambulance_card = findViewById(R.id.ambulance_card);
+
+        commuter_card.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,Login.class);
+                startActivity(intent);
+            }
+        });
+
+        traffic_officer_card.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,Login.class);
+                startActivity(intent);
+            }
+        });
+
+        ambulance_card.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(MainActivity.this,Login.class);
